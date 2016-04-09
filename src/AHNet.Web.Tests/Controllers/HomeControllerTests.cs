7800d72b83d7d0e2c5e56b41a3789b7d28fd13ad
Adam Hilton <adam.tyler.hilton@gmail.com>
2016-04-09 14:46:20 -0500

    added test project for AHNet.Web

---
+++ src/AHNet.Web.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using AHNet.Web.Controllers;
+using Microsoft.AspNet.Mvc;
+
+namespace AHNet.Web.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexReturnViewResult()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+            Assert.IsType(typeof(ViewResult), result);
+        }
+    }
+}
