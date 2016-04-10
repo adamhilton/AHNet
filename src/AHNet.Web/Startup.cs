040f89f6defd4c666dae93b867d14444baddbaf3
Adam Hilton <adam.tyler.hilton@gmail.com>
2016-04-10 11:41:31 -0500

    - Created an admin area inside the web project and defined the routing scheme. - Retargeted the project back from rc1-update2 to rc1-final

--- src/AHNet.Web/Startup.cs
+++ src/AHNet.Web/Startup.cs
@@ -74,7 +74,12 @@ namespace AHNet.Web
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "adminRoute",
+                    template: "{area:exists}/{controller=Dashboard}/{action=Index}");
             });
+
         }
 
         // Entry point for the application.
