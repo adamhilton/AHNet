e36f01db77763d8ded705a7ac0629740cb303bf3
Adam Hilton <adam.tyler.hilton@gmail.com>
2016-09-28 06:38:42 -0500

    - Add tinymce to project - Clean up bundleconfig file

--- src/AHNet.Web/wwwroot/js/clean-blog.js
+++ src/AHNet.Web/wwwroot/js/clean-blog.js
@@ -85,7 +85,7 @@ $('#name').focus(function() {
  // * License: MIT <http://opensource.org/licenses/mit-license.php> - see LICENSE file
  // *
  // * http://ReactiveRaven.github.com/jqBootstrapValidation/
-
+ 
 
 (function( $ ){
 
@@ -557,7 +557,7 @@ $('#name').focus(function() {
                 // How many errors did we find?
                 if (settings.options.semanticallyStrict && errorsFound.length === 1) {
                   // Only one? Being strict? Just output it.
-                  $helpBlock.html(errorsFound[0] +
+                  $helpBlock.html(errorsFound[0] + 
                     ( settings.options.prependExistingHelpBlock ? $helpBlock.data("original-contents") : "" ));
                 } else {
                   // Multiple? Being sloppy? Glue them together into an UL.
