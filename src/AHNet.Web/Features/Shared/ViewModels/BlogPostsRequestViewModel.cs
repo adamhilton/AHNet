7b5688a30ab49b1f6542ac8ac4be9e5552ac0090
Adam Hilton <adam.tyler.hilton@gmail.com>
2016-10-10 07:33:59 -0500

    - User can search for blog post from side bar - Modify site.css

--- src/AHNet.Web/Features/Shared/ViewModels/BlogPostsRequestViewModel.cs
+++ src/AHNet.Web/Features/Shared/ViewModels/BlogPostsRequestViewModel.cs
@@ -6,5 +6,6 @@ namespace AHNet.Web.Features.Shared.ViewModels
         public int page { get; set; } = 1;
         public int pageSize { get; set; } = 20;
         public string tag { get; set; } = string.Empty;
+        public string q { get; set; } = string.Empty;
     }
 }
